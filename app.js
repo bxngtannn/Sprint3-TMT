feca6e537b48b3b8b4c1682f4696a2b2171dd70e
PizarroThiago <pcnthiagosis@gmail.com>
2023-02-26 15:56:01 -0300

    actualizacion de router y app

--- app.js
+++ app.js
@@ -10,19 +10,11 @@ const port = 3030;
 
 app.set('view engine', 'ejs');
 
+app.set('views',path.join('views'))
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {res.render('home')});
+app.get('/', indexRouter);
 
-app.get('/header', (req, res) => {res.render('./partials/header')});
-
-app.get('/login', (req, res) => {res.render('login')})
-
-app.get('/register', (req, res) => {res.render('register')})
-
-app.get('/productCart', (req, res) => {res.render('productCart')})
-
-app.get('/productDetail', (req, res) => {res.render('productDetail')})
 
 app.listen(port, () => console.log(`Servidor funcionando en el puerto ${port}!`));
