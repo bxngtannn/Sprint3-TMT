d8bf82976f14e9e500b278d8abdb222b109406fc
unknown <dxnt3dream@gmail.com>
2023-02-26 01:04:58 -0300

    carpeta partials creada, migración programa a ejs y archivo css ajustado

--- app.js
+++ app.js
@@ -6,18 +6,23 @@ const path = require('path');
 
 const port = 3030;
 
+
+
+app.set('view engine', 'ejs');
+
+
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(path.resolve('./views/home.html')));
+app.get('/', (req, res) => {res.render('home')});
 
-app.get('/header', (req, res) => res.sendFile(path.resolve('./views/header.html')));
+app.get('/header', (req, res) => {res.render('./partials/header')});
 
-app.get('/login', (req, res) => res.sendFile(path.resolve('./views/login.html')))
+app.get('/login', (req, res) => {res.render('login')})
 
-app.get('/register', (req, res) => res.sendFile(path.resolve('./views/register.html')))
+app.get('/register', (req, res) => {res.render('register')})
 
-app.get('/productCart', (req, res) => res.sendFile(path.resolve('./views/productCart.html')))
+app.get('/productCart', (req, res) => {res.render('productCart')})
 
-app.get('/productDetail', (req, res) => res.sendFile(path.resolve('./views/productDetail.html')))
+app.get('/productDetail', (req, res) => {res.render('productDetail')})
 
 app.listen(port, () => console.log(`Servidor funcionando en el puerto ${port}!`));
